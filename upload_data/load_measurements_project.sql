9083ea73888d9f15036b5b39ab2b7103fc6a228c
Alejandra Campo Archbold <42661620+alejandrarchbold@users.noreply.github.com>
2021-05-14 22:05:34 -0500

    Update load_measurements_project.sql

--- upload_data/load_measurements_project.sql
+++ upload_data/load_measurements_project.sql
@@ -13,7 +13,7 @@ from '/home/alejandra/Desktop/sensor_info.csv'
 delimiter ','
 csv header;
 
-SET datestyle = mdy
+SET datestyle = mdy - formato del date 
 
 copy optimum_condition(id_crop_name, crop_name, min_humidity, max_humidity, min_temperature_day,
                       max_temperature_day, min_temperature_night, max_temperature_night, min_ec,
@@ -29,7 +29,7 @@ csv header;
 
 copy crop(id, id_crop_name_optimum_condition, start_date, end_date, id_zone, id_production)
 from '/home/alejandra/Desktop/crop.csv'
-delimiter ',' encoding 'ISO-8859-1'
+delimiter ','
 csv header;
 
 copy sales(id, date, quantity, total_income, id_crop, id_unit_measurement_eqv)
@@ -47,8 +47,10 @@ from '/home/alejandra/Desktop/expenses.csv'
 delimiter ','
 csv header;
 
+SET datestyle = dmy - formato del time de measurement
+
 copy measurement(sensor_id_sensor_info, time, temperature, humidity, pressure, lux,
                 electroconductivity)
 from '/home/alejandra/Desktop/measurement.csv'
-delimiter ',' encoding 'ISO-8859-1'
+delimiter ','
 csv header;
