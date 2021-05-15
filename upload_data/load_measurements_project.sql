e82ca413b86f83d1713d53c30006fae021c4c937
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-05-14 22:14:12 -0500

    actualización del script load_measurements para que funcione con el repo directamente, y añadido de readme

--- upload_data/load_measurements_project.sql
+++ upload_data/load_measurements_project.sql
@@ -1,15 +1,15 @@
 copy zone(id, name)
-from '/home/alejandra/Desktop/zone.csv'
+from '../data/zone.csv'
 delimiter ','
 csv header;
 
 copy unit_measurement_eqv(id, unit, eqv_kg)
-from '/home/alejandra/Desktop/unit_measurement_eqv.csv'
+from '../data/unit_measurement_eqv.csv'
 delimiter ','
 csv header;
 
 copy sensor_info(sensor_id, name, id_zone)
-from '/home/alejandra/Desktop/sensor_info.csv'
+from '../data/sensor_info.csv'
 delimiter ','
 csv header;
 
@@ -18,7 +18,7 @@ SET datestyle = mdy
 copy optimum_condition(id_crop_name, crop_name, min_humidity, max_humidity, min_temperature_day,
                       max_temperature_day, min_temperature_night, max_temperature_night, min_ec,
                       max_ec)
-from '/home/alejandra/Desktop/optimum_condition.csv'
+from '../data/optimum_condition.csv'
 delimiter ','
 csv header;
 
@@ -28,27 +28,27 @@ delimiter ','
 csv header;
 
 copy crop(id, id_crop_name_optimum_condition, start_date, end_date, id_zone, id_production)
-from '/home/alejandra/Desktop/crop.csv'
+from '../data/crop.csv'
 delimiter ',' encoding 'ISO-8859-1'
 csv header;
 
 copy sales(id, date, quantity, total_income, id_crop, id_unit_measurement_eqv)
-from '/home/alejandra/Desktop/sales.csv'
+from '../data/sales.csv'
 delimiter ','
 csv header;
 
 copy expense_category(id, name)
-from '/home/alejandra/Desktop/expense_category.csv'
+from '../data/expense_category.csv'
 delimiter ','
 csv header;
 
 copy expenses(id, date, amount, info, id_expense_category, id_crop)
-from '/home/alejandra/Desktop/expenses.csv'
+from '../data/expenses.csv'
 delimiter ','
 csv header;
 
 copy measurement(sensor_id_sensor_info, time, temperature, humidity, pressure, lux,
                 electroconductivity)
-from '/home/alejandra/Desktop/measurement.csv'
+from '../data/measurement.csv'
 delimiter ',' encoding 'ISO-8859-1'
 csv header;
---
+++ upload_data/readme.md
@@ -0,0 +1,5 @@
+# Creación de la base de datos y cargue de datos SQL
+----
+
+Para el cargue de los datos de la base de datos de prueba usada para este proyecto primero ejecute el script
+*'create_table_project.sql'*, y después proceda a cargar los datos ejecutando el script *'load_measurements_project.sql'*
