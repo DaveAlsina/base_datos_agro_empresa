4f1d4adbf1c4ce554e944d8ff8816ae48a82f49f
alejandrarchbold <alejandra.campo@urosario.edu.co>
2021-05-20 22:59:04 -0500

    dash de sales

--- dashboard/connection/main_sales_expenses_production.py
+++ dashboard/connection/main_sales_expenses_production.py
@@ -7,15 +7,31 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 
-conn = Connection("./conn_data.json")
+conn = Connection("./conn_data.json") #datos para la conexión
 
-#queries
+#estilo de bootstrap
+external_stylesheets = ["https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"]
 
+#instancia de Dash
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#Total de ingresos por cultivo
 conn.openConnection()
-
-print("SALES TABLE")
-query = pd.read_sql_query(sql.getSales(), conn.connection)
+query = pd.read_sql_query(sql.getTotalIncomeByCrop(), conn.connection)
 conn.closeConnection()
-print(query)
-print("\n")
+dfTotalIncomebyCrop = pd.DataFrame(query, columns = ["crop", "total_income"])
+figBarIncome = px.bar(dfTotalIncomebyCrop, x = "crop", y = "total_income")
+
+
+#Layout
+app.layout = html.Div(children = [
+        html.H1(children = 'Sales Analysis'),
+        html.H2(children = 'Total Income by Crop'),
+        dcc.Graph(
+                id = 'barIncome1',
+                figure = figBarIncome),
+        ])
+        
+if __name__ == '__main__':
+    app.run_server(debug = True)
 
--- dashboard/connection/queries.py
+++ dashboard/connection/queries.py
@@ -64,6 +64,6 @@ def getProduction():
     return """select * from production;"""
 
 def getTotalIncomeByCrop():
-    return """select opc.crop_name, cast(sum(sls.total_income) as float) as total_income
-            from sales sls join crop cr on(sls.id_crop = cr.id) join optimum_condition opc on(cr.id_crop_name_optimum_condition = opc.id_crop_name)
-            group by opc.crop_name"""
+    return """select opc.crop_name as crop, cast(sum(sls.total_income) as float) as total_income
+from sales sls join crop cr on(sls.id_crop = cr.id) join optimum_condition opc on(cr.id_crop_name_optimum_condition = opc.id_crop_name)
+group by crop"""
